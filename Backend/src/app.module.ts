ca20e35306bcdcffb290bd1595e5bb842ab3f821
HamzaOubada2 <oubham220@gmail.com>
2026-08-24 02:01:08 +0100

    feat: configure TypeORM with MySQL and global config module

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,4 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
+import { Product } from './products/entities/product.entity';
+import { Order } from './orders/entities/orders.entity';
 @Module({
+    imports: [
+        ConfigModule.forRoot({isGlobal: true}),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => ({
+                type: 'mysql',
+                host: config.get<string>('DB_HOST'),
+                port: config.get<number>('DB_PORT'),
+                username: config.get<string>('DB_USERNAME'),
+                password: config.get<string>('DB_PASSWORD'),
+                database: config.get<string>('DB_DATABASE'),
+                entities: [User, Product, Order],
+                synchronize: true,
+            })
+        })
+    ]
 })
 export class AppModule {}
