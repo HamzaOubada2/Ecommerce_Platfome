9af17b01f25529dc238e02cb2cc9b01fe8f1a176
HamzaOubada2 <oubham220@gmail.com>
2026-08-25 01:13:50 +0100

    feat: implement AuthGuard for JWT authentication and authorization

---
+++ Backend/src/auth/guard/auth.guard.ts
@@ -0,0 +1,34 @@
+import { CanActivate, ExecutionContext, UnauthorizedException } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { JwtService } from "@nestjs/jwt";
+import { Observable } from "rxjs";
+
+
+export class AuthGuard implements CanActivate {
+    constructor(
+        private jwtService: JwtService,
+        private configService: ConfigService,
+    ) {}
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const request = context.switchToHttp().getRequest();
+        const authHeader = request.headers.authorization;
+
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+            throw new UnauthorizedException("No authentication token has been provided");
+        }
+
+        const token = authHeader.split(' ')[1];
+
+        try {
+            const payload = await this.jwtService.verifyAsync(token, {
+                secret: this.configService.get<string>('JWT_ACCESS_SECRET')
+            });
+
+            request.user = payload; // Link Payload With User;
+            return true;
+        } catch (error) {
+            throw new UnauthorizedException("Invalid Or Expired Token!");
+        }
+    }
+}
